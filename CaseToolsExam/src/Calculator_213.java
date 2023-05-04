a1ac1a4c75b326024e4ec408b0cb629ed0ed8367
exam <exam@Bcalab4-m12>
2023-05-04 09:21:12 +0530

    First commit

---
+++ CaseToolsExam/src/Calculator_213.java
@@ -0,0 +1,15 @@
+
+public class Calculator_213 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2;
+		Calculator_213(int num1,int num2){
+			this.num1=num1;
+			this.num2=num2;
+		}
+		
+
+	}
+
+}
